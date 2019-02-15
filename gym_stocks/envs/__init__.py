5ad6327bebb075c60556fde6295a88c884c288a5
Anand <anandj@uchicago.edu>
2019-02-14 22:48:27 -0600

    init

--- gym_stocks/envs/__init__.py
+++
@@ -1,1 +0,0 @@
-from gym_stocks.envs.stocks_env import StocksEnv
--- test.py
+++ test.py
@@ -2,9 +2,8 @@ import gym
 import gym_stocks
 import random
 
-env = gym.make('Stocks-v0')
-print env.reset()
+env = gym.make('Better-v0')
+env.reset()
 
 for i in range(10):
-    print env.step(random.randrange(0, env.action_space.n))
-    #print env.step(0)
+    env.step(random.randrange(0, env.action_space.n))
