fab66238b983c21b3c5908dbb2b7c460ebf46bfc
anandijain <anandj@uchicago.edu>
2019-02-21 14:59:26 -0600

    okay added a little more functionality and the teams, need to add a function that actually outputs that data. need to finish reward function

--- gym_sip/envs/sip_env.py
+++ gym_sip/envs/sip_env.py
@@ -14,13 +14,14 @@ class SippyState:
     def __init__(self, file_name):
         self.fn = file_name + '.csv'
         self.df = None
-        self.headers = [# 'a_team', 'h_team', 'league',
+        self.headers = ['a_team', 'h_team', 'league', 'game_id',
                         'a_pts', 'h_pts', 'secs', 'status', 'a_win', 'h_win', 'last_mod_to_start',
                         'num_markets', 'a_odds_ml', 'h_odds_ml', 'a_hcap_tot', 'h_hcap_tot']
         self.dtypes = {
-                    # 'a_team': 'category',
-                    # 'h_team': 'category',
-                    # 'league': 'category',
+                    'a_team': 'category',
+                    'h_team': 'category',
+                    'league': 'category',
+                    'game_id': 'category',
                     'a_pts': 'Int32',
                     'h_pts': 'Int32',
                     'secs': 'Int32',
@@ -35,8 +36,9 @@ class SippyState:
         }
         self.read_csv()
         self.shape()
-        # self.df.astype(np.float)
-        # self.df.drop(['game_id'], axis=1)
+        self.shape()
+        self.shape()
+        # self.chunk_df()
         # self.fit_data()
         self.index = 0
 
@@ -44,10 +46,18 @@ class SippyState:
 
     def read_csv(self):
         path = 'data/' + self.fn
-        raw = pd.read_csv(path, usecols=self.headers, dtype='Float64')
+        raw = pd.read_csv(path, usecols=self.headers)
         raw.dropna()
-        # raw = pd.get_dummies(data=raw, columns=['a_team', 'h_team', 'league'])
+        raw = pd.get_dummies(data=raw, columns=['a_team', 'h_team', 'league'])
+        # print(raw)
+        # raw = .drop(['a_team', 'h_team', 'league'], axis=1)
         self.df = raw.copy()
+
+    def chunk_df(self):
+        self.df.sort_values(by='game_id', axis=1, inplace=True)
+        self.df.set_index(keys=['game_id'], drop=False, inplace=True)
+        games = self.df['game_id'].unique.tolist()
+        print(games)
 
     def fit_data(self):
         transformer = RobustScaler().fit(self.df)
@@ -60,7 +70,11 @@ class SippyState:
         if self.index >= len(self.df) - 1:
             return None, True
 
-        values = self.df.iloc[self.index, 0:11]
+        values = self.df.iloc[self.index, 0:55]
+
+        cat_vals = values[12:55].to_numpy().nonzero()
+
+        # print(cat_vals)
 
         self.index += 1
 
@@ -69,8 +83,11 @@ class SippyState:
     def shape(self):
         return self.df.shape
 
-    def current_price(self):
+    def a_odds(self):
         return self.df.ix[self.index, 'a_odds_ml']
+
+    def h_odds(self):
+        return self.df.ix[self.index, 'h_odds_ml']
 
 
 class SipEnv(gym.Env):
@@ -96,40 +113,64 @@ class SipEnv(gym.Env):
     def step(self, action):
         assert self.action_space.contains(action)
 
-        portfolio = self.money * self.equity * self.state.current_price()
-        price = self.state.current_price()
-        cost = price * self.num
-        equity_price = price * self.equity
-        prev_portfolio = self.money + equity_price
+        portfolio = self.money + (self.eq_a * self.state.a_odds())
+
+        a_odds = self.state.a_odds()
+        h_odds = self.state.h_odds()
+
+        a_eq = self.eq_calc(a_odds)
+        h_eq = self.eq_calc(h_odds)
+
+        prev_portfolio = self.money
 
         if action == ACTION_BUY_A:
-            if self.money >= 100 * self.num:
+            if self.money >= 100 * self.num and a_odds != 0:
                 self.money -= 100 * self.num
-                self.eq_a += self.num
+                self.eq_a += self.num * a_eq
+            else:
+                print('forced skip')
         if action == ACTION_BUY_H:
-            if self.money >= 100 * self.num:
+            if self.money >= 100 * self.num and h_odds != 0:
                 self.money -= 100 * self.num
-                self.eq_h += self.num
+                self.eq_h += self.num * h_eq
+            else:
+                print('forced skip')
 
         state, done = self.state.next()
-
-        new_price = price
+        print(act_name(action))
+        new_price = a_odds - h_odds
         if not done:
-            new_price = self.state.current_price()
+            new_price = self.state.a_odds()
 
-        new_equity_price = new_price * self.equity
+        new_equity_price = new_price * self.eq_a
         reward = (self.money + new_equity_price) - prev_portfolio
-
+        print('reward: ' + str(reward))
         return state, reward, done, None
+
+    def eq_calc(self, odd):
+        if odd >= 100:
+            return odd/100.
+        elif odd < 100:
+            return abs(100/odd)
 
     def reset(self):
         self.state = SippyState(random.choice(self.states))
 
-        self.money = 3500
-        self.equity = 0
+        self.money = 10000
+        self.eq_a = 0
+        self.eq_h = 0
 
         state, done = self.state.next()
         return state
 
     def _render(self, mode='human', close=False):
         pass
+
+
+def act_name(act):
+    if act == 0:
+        return 'SKIP'
+    elif act == 1:
+        return 'BUY AWAY'
+    elif act == 2:
+        return 'BUY HOME'
--- test.py
+++ test.py
@@ -5,5 +5,8 @@ import gym_sip
 env = gym.make('Sip-v0')
 print(env.reset())
 
-for i in range(1000):
-    env.step(random.randrange(0, env.action_space.n))
+for i in range(1):
+    s, r, d, m = env.step(random.randrange(0, env.action_space.n))
+    # print(s['secs', 'a_pts', 'h_pts', ])
+    # print(s)
+
