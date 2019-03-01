d1f9932b3c15ec01beab3bb7fc8413ef3bb40ddd
Anand <anandj@uchicago.edu>
2019-02-28 21:26:54 -0600

    cleaning for readability, fixed the game.id

--- gym_sip/envs/sip_env.py
+++ gym_sip/envs/sip_env.py
@@ -6,19 +6,13 @@ from sklearn.preprocessing import RobustScaler
 
 ACTION_SKIP = 0
 ACTION_BUY_A = 1
-# ACTION_BUY_H = 2
-
 
 class SippyState:
     def __init__(self, game):
-        # print(game)
         self.game = game  # df for game
-        # self.win = game['a_win'].unique()[0]
         self.id = self.game['game_id'].iloc[0]
-
-        # self.id = self.game.iloc[:, 2].unique().tolist()  # first row, second column
+        # if len()
         self.index = 0
-
         print("Imported data from {}".format(self.id))
 
     def fit_data(self):
@@ -93,13 +87,13 @@ class SipEnv(gym.Env):
         self.bet_amt = 100
         self.money = 0  # DOESN'T MATTER IF IT RUNS OUT OF MONEY AND MAX BETS IS HELD CONSTANT
         self.bound = 16
-
+        self.adj_a_odds = 0
+        self.adj_h_odds = 0
         self.eq_a = 0
         self.eq_h = self.bet_amt * self.adj_h_odds - self.bet_amt
         self.a_odds = 0
         self.h_odds = 0
-        self.adj_a_odds = 0
-        self.adj_h_odds = 0
+
         self.a_bet_amt = 0
 
         self.state = None
@@ -112,16 +106,12 @@ class SipEnv(gym.Env):
 
     def step(self, action):
         assert self.action_space.contains(action)
-        reward = 0
-        portfolio = self.money
         prev_portfolio = self.money
 
         state, done = self.state.next()
 
         if not done:
             self.get_odds()
-        # if done:
-        #     self.money -= self.bet_amt
 
         self.actions(action)
 
@@ -138,11 +128,27 @@ class SipEnv(gym.Env):
                 print('a')
         if action == ACTION_SKIP:
             print('s')
-        # if action == ACTION_BUY_H:
-        #     if self.h_odds != 0 or self.h_bet_count < self.max_bets:
-        #         self.money -= self.bet_amt
-        #         self.eq_h += self.adj_h_odds
-        #         self.h_bet_count += 1
+
+    def next(self):
+        new_id = random.choice(self.ids)
+        self.state = SippyState(self.states[new_id])
+
+        self.eq_a = 0
+        self.eq_h = self.bet_amt * self.adj_h_odds - self.bet_amt
+        self.money -= self.
+        state, done = self.state.next()
+        return state
+
+    def reset(self):
+        new_id = random.choice(self.ids)
+        self.state = SippyState(self.states[new_id])
+
+        self.money = 0
+        self.eq_a = 0
+        self.eq_h = self.bet_amt * self.adj_h_odds - self.bet_amt
+
+        state, done = self.state.next()
+        return state
 
     def read_csv(self):
         raw = pd.read_csv(self.fn, usecols=self.headers)
@@ -161,27 +167,6 @@ class SipEnv(gym.Env):
         self.h_odds = self.state.h_odds()
         self.adj_a_odds = eq_calc(self.a_odds)
         self.adj_h_odds = eq_calc(self.h_odds)
-
-    def next(self):
-        new_id = random.choice(self.ids)
-        self.state = SippyState(self.states[new_id])
-
-        self.eq_a = 0
-        self.eq_h = self.bet_amt * self.adj_h_odds - self.bet_amt
-
-        state, done = self.state.next()
-        return state
-
-    def reset(self):
-        new_id = random.choice(self.ids)
-        self.state = SippyState(self.states[new_id])
-
-        self.money = 0
-        self.eq_a = 0
-        self.eq_h = self.bet_amt * self.adj_h_odds - self.bet_amt
-
-        state, done = self.state.next()
-        return state
 
     def _render(self, mode='human', close=False):
         pass
